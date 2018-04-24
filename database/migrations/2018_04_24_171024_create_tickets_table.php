2f79b9d5540a29732136a79425d1bb2e0373df4d
duy <vanduy07c.r@gmail.com>
2018-04-25 00:13:16 +0700

    fix database type of description ticket

--- database/migrations/2018_04_24_171024_create_tickets_table.php
+++ database/migrations/2018_04_24_171024_create_tickets_table.php
@@ -17,7 +17,7 @@ class CreateTicketsTable extends Migration
             $table->increments('id');
             $table->integer('user_id');
             $table->string('title');
-            $table->string('description');
+            $table->text('description');
             $table->timestamps();
         });
     }
